d5834b05dc891c0fce3acc0a33fef12db2c2e7e1
prodesigner2013-sys <prodesigner2013@gmail.com>
2026-03-01 13:07:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Ouvrir PDF
 function openPDF(file){
-    window.open(file, '_blank');
+    window.location.href = file;
 }
-
 // Preloader
 window.addEventListener("load", function(){
     setTimeout(function(){
         document.getElementById("preloader").classList.add("hidden");
     }, 2000);
+
 });
